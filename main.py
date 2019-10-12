847c1f871ac661b3e0add1b1e731edfee261a363
yedukrishnan.h <yedukrishnan.h@gmail.com>
2019-10-12 15:47:00 +0530

    Added Testing

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ import csvconv
 #inputs 
 lr=float(input("Input Learning Rate:"))
 Epochs=int(input("Input Number Of Epochs:"))
-
-floatdata=csvconv.tofloat(sys.argv[1])
-weights=perceptron.train(floatdata,Epochs,lr)
-
+#training
+floatdata=csvconv.tofloat('train.csv')
+weights=perceptron.train(floatdata,lr,Epochs)
 print("Weights\n ",weights)
+#testing
+floattest=csvconv.tofloat('test.csv')
+acc=perceptron.test(floattest,weights)
+print("Accuracy=",acc)
--- perceptron.py
+++ perceptron.py
@@ -1,17 +1,42 @@
-def train(floatdata,Epochs,lr):
+#training
+def train(floatdata,lr,Epochs):
     weights = [0.0 for i in range(len(floatdata[0]))]
+    correct= False
+   ## while(correct==False):
     for i in range(Epochs):
+        correct=True
+        print(weights[0])
         for instance in floatdata:
-                r = weights[0]
-                for attribute in range(len(weights)-1):
-                    r += weights[attribute+1] * instance[attribute]
-                if(r>=0):
-                    calculated=1.0
-                else:
-                    calculated=-1.0
-                if calculated != instance[-1]:
-                    error = instance[-1] - calculated
-                    weights[0] += lr * error
-                    for i in range(len(weights)-1):
-                        weights[i+1] += instance[i] * lr * error
+            ye=weights[0]
+            for attribute in range(len(weights)-1):
+                ye +=weights[attribute+1]*instance[attribute]
+            if ye>=0:
+                ye=1.0
+            else:
+                ye=-1.0
+            if ye!=instance[-1]:
+                correct=False
+                weights[0] += lr * (instance[-1] - ye)
+                for k in range(len(weights)-1):
+                    weights[k+1] += instance[k] * lr * (instance[-1] - ye)
+        if correct:
+            break
+    print('Epochs Done=',i+1)
     return weights
+#testing
+def test(floattest,weights):
+    r=0
+    for instance in floattest:
+        ye=weights[0]
+        for attribute in range(len(weights)-1):
+            ye +=weights[attribute+1]*instance[attribute]
+        if ye>=0:
+            ye=1.0
+        else:
+            ye=-1.0
+        if ye!=instance[-1]:
+            r-=1
+        else:
+            r+=1
+    acc=(r/len(floattest))*100
+    return acc
