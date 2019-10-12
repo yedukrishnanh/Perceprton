2312e56bd25a240289f1e9d0ea685edc18753227
yedukrishnan.h <yedukrishnan.h@gmail.com>
2019-10-12 17:58:28 +0530

    final

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import csv
 import perceptron
 import csvconv
+import sys
 
 #inputs 
 lr=float(input("Input Learning Rate:"))
 Epochs=int(input("Input Number Of Epochs:"))
 #training
-floatdata=csvconv.tofloat('train.csv')
+floatdata=csvconv.tofloat(sys.argv[1])
 weights=perceptron.train(floatdata,lr,Epochs)
 print("Weights\n ",weights)
 #testing
-floattest=csvconv.tofloat('test.csv')
+floattest=csvconv.tofloat(sys.argv[2])
 acc=perceptron.test(floattest,weights)
 print("Accuracy=",acc)
